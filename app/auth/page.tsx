261628449b560f999a3590d24d9179a09c46a546
On Thi Thu Hanh <hanhotthe153752@fpt.edu.vn>
2025-07-11 00:00:31 +0700

    fix authen

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -16,7 +16,7 @@ export default function auth() {
         try {
             const response = await authApi.login({email, password})
             localStorage.setItem("user", JSON.stringify(response)) 
-            console.log(response)               
+            //console.log(response)               
             router.push("/")   
         } catch (error) {            
             setAuthMessage("Đăng nhập thất bại: Email hoặc mật khẩu không đúng")            
